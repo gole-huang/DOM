8dfd55852b2abe5f62dc8cf80118b6a88685f9dd
gole.huang <gole.huang@gmail.com>
2022-04-14 00:52:19 +0800

    keyboard supported

--- script/script.js
+++ script/script.js
@@ -17,16 +17,15 @@ function addOnLoad(event) {
 function js_after_load() {
 	try {
 		let picList = document.getElementById("showPic").getElementsByTagName("a");	//DOM方法两个get不能分开写
-		/*	//IE 11不支持for of循环，只支持for i++循环；
-		for (let pic of picList) {
-			pic.onclick = function () {
-				return show_pic(this);
-			};
-		}
-		*/
+		//IE 11不支持for of循环，只支持for循环；
 		for (let i = 0 ; i < picList.length; i++) {
 			picList[i].onclick = function () {
 				return show_pic(this);
+			}
+			picList[i].onkeydown = function (event) {
+				if(event.key == " ") {
+					return show_pic(this);
+				}
 			}
 		}
 	} catch (err) {
